e8109db1905e497010c0f56fff14235a73727d40
C. S. F. Junior <csf.junior90@gmail.com>
2019-05-13 10:38:20 -0300

    Models + Test GET

--- src/doghouse/models.py
+++ src/doghouse/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+import os
+
+def get_image_path(instance, filename):
+    return os.path.join('pics', str(instance.id), filename)
 
 # Create your models here.
+class Pets(models.Model):
+    pet = models.ForeignKey('PET', unique=True, models.SET_NULL, blank=True, null=True)
+    pet_foto = ImageField(upload_to=get_image_path, blank=True, null=True)
+
+    CACHORRO = 'C'
+    GATO = 'G'
+    ESPECIE_CHOICES = (
+        (CACHORRO, 'C'),
+        (GATO, 'G')
+    )
+    especie = models.CharField(max_length=1, choices=ESPECIE_CHOICES, default=CACHORRO)
+
+    PEQ = 'Pq'
+    MED = 'Md'
+    GDE = 'Gd'
+    PORTE_CHOICES = (
+        (PEQ, 'Pequeno'),
+        (MED, 'Médio'),
+        (GDE, 'Grande')
+    )
+    porte = models.CharField(max_length=2, choices=PORTE_CHOICES, default=GDE)
+
+    FILHOTE = 'F'
+    ADULTO = 'A'
+    IDADE_CHOICES = (
+        (FILHOTE, 'Filhote'),
+        (ADULTO, 'Adulto')
+    )
+
+    nome = models.CharField(max_length=50, null=False)
+    idade = models.CharField(max_length=1, choices=IDADE_CHOICES, default=ADULTO)
+    raca = models.CharField(max_length=100, null=False)
+    obs = models.TextField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return "pet_foto: {}\nEspecie: {}\nPorte: {}\nNome: {}\nIdade: {}\nRaça: {}\nObs.: {}"\
+        .format(self.pet_foto, self.especie, self.porte, self.nome, self.idade, self.raca, self.obs)
--- src/doghouse/tests.py
+++ src/doghouse/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from .models import Pets
+from .serializers import PetsSerializer
 
 # Create your tests here.
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    @staticmethod
+    def create_pet(pet_foto=None, especie="", porte="", nome="", idade="", raca="", obs=""):
+        Pets.objects.create(pet_foto=pet_foto, especie=especie, porte=porte, nome=nome, idade=idade, raca=raca, obs=obs)
+
+    def setUp(self):
+        # add test data
+        self.create_pet(None, 'C', 'Gd', "Rex", 'A', "Vira-lata", "Cão adulto super dócil")
+        self.create_pet(None, 'G', 'Pq', "Salem", 'F', "Vira-lata", "Filhote não vacinado")
+        self.create_pet(None, 'C', 'Md', "Toby", 'F', "Vira-lata", "Filhote vacinado - 40 dias de vida")
+
+class GetAllPets(BaseViewTest):
+    def test_get_all_pets(self):
+        response = self.client.get(
+            reverse("pets-all", kwargs={"version": "v1"})
+        )
+        expected = Pets.objects.all()
+        serialized = PetsSerializer(expected, many=True)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
